17ee66e6f00d80d41db769ce85b775f0674e4375
Nikolai Faustov <61990876+nfaustov@users.noreply.github.com>
2021-09-28 13:27:29 +0300

    Added token verification and refreshing in AuthorizationInteractor

--- PDFDocumentBuilder/Modules/AuthorizationModule/Interactor/AuthorizationInteractor.swift
+++ PDFDocumentBuilder/Modules/AuthorizationModule/Interactor/AuthorizationInteractor.swift
@@ -22,12 +22,64 @@ final class AuthorizationInteractor {
 
 extension AuthorizationInteractor: AuthorizationInteraction {
     func getToken() {
-        authorizationService?.getToken()
+        if let tokenEntity = authorizationDatabase?.getToken() {
+            let token = Token(usingEntity: tokenEntity)
+            verifyToken(token: token)
+        } else {
+            authorizationService?.getToken()
+                .receive(on: RunLoop.main)
+                .sink(receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        print("Couldn't get token: \(error.localizedDescription)")
+                    case .finished: break
+                    }
+                }, receiveValue: { [delegate, authorizationDatabase] response in
+                    if let token = response.token {
+                        authorizationDatabase?.saveToken(token: token)
+                        delegate?.tokenDidRecieved()
+                    } else if let errorMessage = response.errorMessage {
+                        delegate?.tokenFailure(message: errorMessage)
+                    } else {
+                        delegate?.tokenFailure(message: "Ошибка запроса токена")
+                    }
+                })
+                .store(in: &subscriptions)
+        }
+    }
+}
+
+// MARK: - Private extension
+
+private extension AuthorizationInteractor {
+    func verifyToken(token: Token) {
+        authorizationService?.verifyToken(token: token)
             .receive(on: RunLoop.main)
             .sink(receiveCompletion: { completion in
                 switch completion {
                 case .failure(let error):
-                    print("Couldn't get token: \(error.localizedDescription)")
+                    print("Couldn't verify token: \(error.localizedDescription)")
+                case .finished: break
+                }
+            }, receiveValue: { [delegate, refreshToken] verification in
+                if let valid = verification.detail?.valid {
+                    valid ? delegate?.tokenDidRecieved() : refreshToken(token)
+                } else if let errorMessage = verification.errorMessage {
+                    delegate?.tokenFailure(message: errorMessage)
+                } else {
+                    delegate?.tokenFailure(message: "Ошибка запроса при верификации токена")
+                }
+            })
+            .store(in: &subscriptions)
+    }
+
+    func refreshToken(token: Token) {
+        authorizationService?.refreshToken(token: token)
+            .receive(on: RunLoop.main)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    print("Couldn't refresh token: \(error.localizedDescription)")
                 case .finished: break
                 }
             }, receiveValue: { [delegate, authorizationDatabase] response in
@@ -36,6 +88,8 @@ extension AuthorizationInteractor: AuthorizationInteraction {
                     delegate?.tokenDidRecieved()
                 } else if let errorMessage = response.errorMessage {
                     delegate?.tokenFailure(message: errorMessage)
+                } else {
+                    delegate?.tokenFailure(message: "Ошибка за проса при обновлении токена")
                 }
             })
             .store(in: &subscriptions)
