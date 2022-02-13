68649fa69e10882ab02cfe0eb32fed3bd9ebffbe
Nikolai Faustov <61990876+nfaustov@users.noreply.github.com>
2022-02-13 13:26:17 +0300

    Separated ContractData into different files

---
+++ PDFDocumentBuilder/Models/Patient.swift
@@ -0,0 +1,69 @@
+//
+//  Patient.swift
+//  PDFDocumentBuilder
+//
+//  Created by Nikolai Faustov on 13.02.2022.
+//
+
+import Foundation
+
+final class Patient {
+    let id: UUID?
+    let passport: PassportData
+    let placeOfResidence: PlaceOfResidence
+
+    var name: String {
+        passport.surname + " " + passport.name + " " + passport.patronymic
+    }
+
+    var residence: String {
+        let items = [
+            placeOfResidence.region,
+            placeOfResidence.locality,
+            placeOfResidence.streetAdress,
+            placeOfResidence.house,
+            placeOfResidence.appartment
+        ]
+        var residence = ""
+
+        for item in items where !item.isEmpty {
+            residence.append(item + ", ")
+        }
+
+        if residence.count > 1 { residence.removeLast(2) }
+
+        return residence
+    }
+
+    init(id: UUID? = UUID(), passport: PassportData, placeOfResidence: PlaceOfResidence) {
+        self.id = id
+        self.passport = passport
+        self.placeOfResidence = placeOfResidence
+    }
+}
+
+struct PassportData: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case name, surname, patronymic, gender, birthday, birthplace, authority
+        case seriesNumber = "series_number"
+        case issueDate = "issue_date"
+    }
+
+    let name: String
+    let surname: String
+    let patronymic: String
+    let gender: String
+    let seriesNumber: String
+    let birthday: String
+    let birthplace: String
+    let issueDate: String
+    let authority: String
+}
+
+struct PlaceOfResidence {
+    let region: String
+    let locality: String
+    let streetAdress: String
+    let house: String
+    let appartment: String
+}
--- PDFDocumentBuilder/Models/PriceList.swift
+++ PDFDocumentBuilder/Models/PriceList.swift
@@ -1,46 +1,11 @@
 //
-//  ContractData.swift
+//  PriceList.swift
 //  PDFDocumentBuilder
 //
-//  Created by Nikolai Faustov on 28.07.2021.
+//  Created by Nikolai Faustov on 13.02.2022.
 //
 
 import Foundation
-
-final class Patient {
-    let id: UUID?
-    let passport: PassportData
-    let placeOfResidence: PlaceOfResidence
-
-    var name: String {
-        passport.surname + " " + passport.name + " " + passport.patronymic
-    }
-
-    var residence: String {
-        let items = [
-            placeOfResidence.region,
-            placeOfResidence.locality,
-            placeOfResidence.streetAdress,
-            placeOfResidence.house,
-            placeOfResidence.appartment
-        ]
-        var residence = ""
-
-        for item in items where !item.isEmpty {
-            residence.append(item + ", ")
-        }
-
-        if residence.count > 1 { residence.removeLast(2) }
-
-        return residence
-    }
-
-    init(id: UUID? = UUID(), passport: PassportData, placeOfResidence: PlaceOfResidence) {
-        self.id = id
-        self.passport = passport
-        self.placeOfResidence = placeOfResidence
-    }
-}
 
 final class PriceList {
     private lazy var categories: [ServicesCategory] = {
@@ -105,30 +70,4 @@ struct Service: Decodable, Hashable {
 
         return title.lowercased().contains(lowercasedFilter)
     }
-}
-
-struct PassportData: Decodable {
-    enum CodingKeys: String, CodingKey {
-        case name, surname, patronymic, gender, birthday, birthplace, authority
-        case seriesNumber = "series_number"
-        case issueDate = "issue_date"
-    }
-
-    let name: String
-    let surname: String
-    let patronymic: String
-    let gender: String
-    let seriesNumber: String
-    let birthday: String
-    let birthplace: String
-    let issueDate: String
-    let authority: String
-}
-
-struct PlaceOfResidence {
-    let region: String
-    let locality: String
-    let streetAdress: String
-    let house: String
-    let appartment: String
 }
